9e2c2dd01809915a2d2fa8ba7382f9929c0ae883
Alex Church <ac14293@bristol.ac.uk>
2023-02-28 10:50:52 +0000

    add skeletonize peak detector update contourblob detector for latest opencv >= 4.6 add/update optimization of detector params

--- examples/doh_blob_detector_test.py
+++ examples/doh_blob_detector_test.py
@@ -1,27 +1,44 @@
 # -*- coding: utf-8 -*-
-"""Test script for OpenCV contour blob detector (including parameter optimization).
+"""Test script for scikit doh blob detector (including parameter optimization).
 """
 
 import cv2
 import numpy as np
 
 from vsp.video_stream import CvVideoCamera, CvVideoDisplay
-from vsp.detector import SklDoHBlobDetector
+from vsp.detector import SklDoHBlobDetector, optimize_doh_blob_detector_params
 
 
 def main():
-    det = SklDoHBlobDetector()
+    # with CvVideoCamera(source=1, api_name='DSHOW', is_color=False) as inp, CvVideoDisplay() as out: # Windows
+    with CvVideoCamera(source=8, frame_size=(640, 480), is_color=False) as inp, CvVideoDisplay() as out:  # Linux
+        out.open()
+        frames = []
+        for i in range(300):
+            frame = inp.read()
+            out.write(frame)
+            frames.append(frame)
+        frames = np.array(frames)
 
-    with CvVideoCamera(source=1, api_name='DSHOW', is_color=False) as inp, CvVideoDisplay() as out:
+    idx = np.random.choice(frames.shape[0], 10, replace=False)
+    frames = frames[idx]
+
+    params = optimize_doh_blob_detector_params(frames,
+                                               target_blobs=331,
+                                               min_sigma_range=(1.0, 10.0),
+                                               max_sigma_range=(1.0, 10.0),
+                                               num_sigma_range=(1, 10),
+                                               threshold_range=(0.0, 0.1),
+                                               )
+    print(params)
+    det = SklDoHBlobDetector(**params)
+
+    # with CvVideoCamera(source=1, api_name='DSHOW', is_color=False) as inp, CvVideoDisplay() as out: # Windows
+    with CvVideoCamera(source=8, frame_size=(640, 480), is_color=False) as inp, CvVideoDisplay() as out:  # Linux
         out.open()
         while True:
             frame = inp.read()
             keypoints = det.detect(frame)
-            pts = np.array([f.point for f in keypoints])
-            sizes = np.array([f.size for f in keypoints])
-            print("pts.shape = {}".format(pts.shape))
-            print("sizes.shape = {}".format(sizes.shape))
-
             kpts = [cv2.KeyPoint(kp.point[0], kp.point[1], 2 * kp.size) for kp in keypoints]
             frame_with_kpts = cv2.drawKeypoints(frame, kpts, np.array([]), (0, 0, 255),
                                                 cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
